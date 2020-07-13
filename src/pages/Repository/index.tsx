b7aeea0c9a1fe5008b9884eced0865e4cbdb3c60
Felipe Monteiro <felipedeamonteiro@gmail.com>
2020-07-13 00:48:11 -0300

    Finish styled components part

--- src/pages/Repository/index.tsx
+++ src/pages/Repository/index.tsx
@@ -1,7 +1,64 @@
 import React from 'react';
+import { useRouteMatch, Link } from 'react-router-dom';
+import { FiChevronLeft, FiChevronRight } from 'react-icons/fi';
+
+import logoImg from '../../assets/logo.svg';
+
+import { Header, RepositoryInfo, Issues } from './styles';
+
+interface RepositoryParams {
+  repository: string;
+}
 
 const Repository: React.FC = () => {
-  return <h1>Repository</h1>;
+  const { params } = useRouteMatch<RepositoryParams>();
+  return (
+    <>
+      <Header>
+        <img src={logoImg} alt="GithubExplorer" />
+        <Link to="/">
+          <FiChevronLeft size={16} />
+          Voltar
+        </Link>
+      </Header>
+      <RepositoryInfo>
+        <header>
+          <img
+            src="https://avatars0.githubusercontent.com/u/28929274?v=4"
+            alt="Rocketseat"
+          />
+          <div>
+            <strong>rocketseat/unform</strong>
+            <p>descrição do repositório</p>
+          </div>
+        </header>
+        <ul>
+          <li>
+            <strong>1808</strong>
+            <p>Stars</p>
+          </li>
+          <li>
+            <strong>48</strong>
+            <p>Forks</p>
+          </li>
+          <li>
+            <strong>67</strong>
+            <p>Issues abertas</p>
+          </li>
+        </ul>
+      </RepositoryInfo>
+
+      <Issues>
+        <Link to="fgdfgdfgd">
+          <div>
+            <strong>ioajsasjaio</strong>
+            <p>sdksodakopdkaop</p>
+          </div>
+          <FiChevronRight size={20} />
+        </Link>
+      </Issues>
+    </>
+  );
 };
 
 export default Repository;
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -7,8 +7,10 @@ import Repository from '../pages/Repository';
 const Routes: React.FC = () => (
   <Switch>
     <Route path="/" exact component={Dashboard} />
-    <Route path="/repository" exact component={Repository} />
+    <Route path="/repository/:repository+" exact component={Repository} />
   </Switch>
 );
+
+// Esse '+' que tem na url é pra falar que o "nome do repository" é tudo o que tem depois da barra
 
 export default Routes;
